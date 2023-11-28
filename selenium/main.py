1f5d0d9a9eee32f9158476b9ce330ae7c6907dce
stevenyang <steventanyang@gmail.com>
2023-11-28 01:33:27 -0500

    set up chromedriver

--- selenium/main.py
+++ selenium/main.py
@@ -1,6 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+import time
 
-service = Service(executable_path="")
+service = Service(executable_path="/Users/stevenyang/projects/harden/selenium/chromedriver")
 driver = webdriver.Chrome(service=service)
 
+driver.get("https://www.cbssports.com/fantasy/football/")
+
+time.sleep(10)
+
+driver.quit()
