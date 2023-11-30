79bde2d269d5b5a4375e9de8a23437e7a2a15aaf
stevenyang <steventanyang@gmail.com>
2023-11-29 23:48:01 -0500

    updated

--- selenium/main.py
+++ selenium/main.py
@@ -11,17 +11,68 @@ import time
 service = Service(executable_path="/Users/stevenyang/projects/harden/selenium/chromedriver")
 driver = webdriver.Chrome(service=service)
 
-driver.get("https://www.google.com/")
+# driver.get("https://www.statmuse.com/nba")
 
-
-# input_element = WebDriverWait(driver, 10).until(
-#     EC.element_to_be_clickable((By.CLASS_NAME, "gLFyf"))
+# WebDriverWait(driver, 5).until(
+# 	EC.presence_of_element_located((By.NAME, "question[query]"))
 # )
-# print('yes')
 
-input_element = driver.find_element(By.CLASS_NAME, "gLFyf")
+# input_element = driver.find_element(By.NAME, "question[query]")
 
-input_element.send_keys("Lebron James" + Keys.ENTER)
+# input_element.send_keys("Lebron James stats this season" + Keys.ENTER)
 
-time.sleep(10)
-driver.quit()
+# time.sleep(10)
+# driver.quit()
+
+def statmuseSearch(website, search):
+
+    driver.get(website) #this gets the website
+
+    WebDriverWait(driver, 5).until( #we're waiting until everything loads
+	    EC.presence_of_element_located((By.NAME, "question[query]")) 
+    )
+
+    input_element = driver.find_element(By.NAME, "question[query]") #finds the search bar
+    input_element.send_keys(search + Keys.ENTER) #enters the search
+
+    #click on link
+    # WebDriverWait(driver, 10).until( #we're waiting until everything loads
+	#     EC.presence_of_element_located((By.LINK_TEXT, "LeBron James")) 
+    # )
+    # link = driver.find_element(By.LINK_TEXT, "LeBron James")
+    # link.click()
+
+    WebDriverWait(driver, 5).until( #we're waiting until everything loads
+	    EC.presence_of_element_located((By.XPATH, "//p[contains(@class, 'my-[1em]') and contains(@class, 'underline') and contains(@class, 'text-team-secondary')]")) 
+    )
+    print('found')
+
+    element = driver.find_element(By.XPATH, "//p[contains(@class, 'my-[1em]') and contains(@class, 'underline') and contains(@class, 'text-team-secondary')]")
+    
+    time.sleep(10)
+    driver.quit()
+
+    return element
+
+def injuryReport(website):
+
+    driver.get(website) #this gets the website
+
+    WebDriverWait(driver, 5).until( #we're waiting until everything loads
+	    EC.presence_of_element_located((By.XPATH, "//item/description")) 
+    )
+
+    descriptions = driver.find_elements(By.XPATH, "//item/description")
+
+    for description in descriptions:
+        print(description.text)
+    
+    time.sleep(10)
+    driver.quit()
+
+    return descriptions
+
+
+# print(statmuseSearch("https://www.statmuse.com/nba", "lebron james points"))
+injuries = injuryReport("https://www.rotowire.com/rss/news.php?sport=NBA")
+
