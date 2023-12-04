21427199a6d9c3fc5967880f978b95ca9f1b8987
stevenyang <steventanyang@gmail.com>
2023-12-03 23:57:07 -0500

    its not working

--- selenium/scrape.py
+++ selenium/scrape.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
 
 import time
@@ -51,14 +52,14 @@ def pullEspnLeague(website, email):
     link = driver.find_element(By.LINK_TEXT, "SIGN UP")
     link.click()
 
-    WebDriverWait(driver, 5).until( #we're waiting until everything loads
-	    EC.presence_of_element_located((By.XPATH, "//button[@id='BtnSubmit' and @type='submit']")) 
+    WebDriverWait(driver, 10).until( #we're waiting until everything loads
+	    EC.presence_of_element_located((By.XPATH, "//*[text()='Email']")) 
     )
-    submit_button = driver.find_element(By.XPATH, "//button[@id='BtnSubmit' and @type='submit']")
-    submit_button.click()
+    print('found')
+    element = driver.find_element(By.XPATH, "//*[text()='Email']")
+    element.click()
 
-    # input_element.send_keys(email + Keys.ENTER) #enters the search
-
+    ActionChains(driver).send_keys(email).perform()
 
     time.sleep(10)
     driver.quit()
